3c594663909a5dee71a9657f16a902f0a458d086
Eduardo Lombardi <eduardoribeiro@brq.com>
2018-04-18 15:35:48 -0300

    Adicionado layout e código de captura da câmera para o projeto

--- CoreML-Project/ViewController.swift
+++ CoreML-Project/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var uiRecognitionLabel: UILabel?
+    
+    @IBOutlet weak var uiCameraView: UIView?
 
+    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+
+    
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+            else { return session }
+        session.addInput(input)
+        return session
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.uiCameraView?.layer.addSublayer(self.cameraLayer)
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyCameraQueue"))
+        self.captureSession.addOutput(videoOutput)
+        self.captureSession.startRunning()
+
+       
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.cameraLayer.frame = self.uiCameraView?.bounds ?? .zero
     }
 
     override func didReceiveMemoryWarning() {
