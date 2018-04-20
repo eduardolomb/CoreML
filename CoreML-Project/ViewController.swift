cc30039305af85aa776f723664e427c92fea6f49
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2018-04-19 22:15:19 -0300

    Adicionado código de rastreio de imagens

--- CoreML-Project/ViewController.swift
+++ CoreML-Project/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
+
 
 class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate {
     
     @IBOutlet weak var uiRecognitionLabel: UILabel?
     
     @IBOutlet weak var uiCameraView: UIView?
-
+    private var requests = [VNRequest]()
     private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
 
     
@@ -36,9 +39,37 @@ class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDeleg
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyCameraQueue"))
         self.captureSession.addOutput(videoOutput)
         self.captureSession.startRunning()
-
+        setupVision()
        
     }
+    
+    
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: imageClassifier().model)
+            else { fatalError("Can't load VisionML model") }
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+        self.requests = [classificationRequest]
+    }
+    
+    func handleClassifications(request: VNRequest, error: Error?) {
+        guard let observations = request.results
+            else { print("no results: \(error!)"); return }
+        let classifications = observations[0...4]
+            .flatMap({ $0 as? VNClassificationObservation })
+            .filter({ $0.confidence > 0.3 })
+            .sorted(by: { $0.confidence > $1.confidence })
+            .map {
+                (prediction: VNClassificationObservation) -> String in
+                return "\(round(prediction.confidence * 100 * 100)/100)%: \(prediction.identifier)"
+        }
+        DispatchQueue.main.async {
+            print(classifications.joined(separator: "###"))
+            self.uiRecognitionLabel?.text = classifications.joined(separator: "\n")
+        }
+    }
+    
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.cameraLayer.frame = self.uiCameraView?.bounds ?? .zero
