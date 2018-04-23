e302b361d55f622ce9a99fd5583738f8bab4e393
Eduardo Lombardi <eduardoribeiro@brq.com>
2018-04-23 15:40:11 -0300

    Adicionado Alamofire e possibilidade de troca do modelo via web.

--- CoreML-Project/ViewController.swift
+++ CoreML-Project/ViewController.swift
@@ -10,27 +10,38 @@ import UIKit
 import AVFoundation
 import CoreML
 import Vision
-
+import Alamofire
 
 class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate {
 
     @IBOutlet weak var uiRecognitionLabel: UILabel?
+    @IBOutlet weak var uiSlider: UISlider?
+    @IBOutlet weak var uiDownloadLabel: UILabel?
     
+    @IBOutlet weak var uiSwitch: UISwitch?
     @IBOutlet weak var uiCameraView: UIView?
     private var requests = [VNRequest]()
     private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
 
+    var downloaded = false
     
-    private lazy var captureSession: AVCaptureSession = {
-        let session = AVCaptureSession()
-        session.sessionPreset = AVCaptureSession.Preset.photo
-        guard
-            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
-            let input = try? AVCaptureDeviceInput(device: backCamera)
-            else { return session }
-        session.addInput(input)
-        return session
-    }()
+    
+    //MARK: - Interface Buttons Functions
+    
+    @IBAction func downloadBtnTap(_ sender: Any) {
+        self.captureSession.stopRunning()
+        downloadModel()
+    }
+    
+    @IBAction func validateSwitchTap(_ sender: UISwitch) {
+        if sender.isOn {
+            self.captureSession.startRunning()
+        } else {
+            self.captureSession.stopRunning()
+        }
+    }
+    
+    //MARK: - ViewControllerDelegates
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,20 +49,18 @@ class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDeleg
         let videoOutput = AVCaptureVideoDataOutput()
         videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyCameraQueue"))
         self.captureSession.addOutput(videoOutput)
-        self.captureSession.startRunning()
+        uiSwitch?.setOn(false, animated: false)
         setupVision()
        
     }
     
-    
-    func setupVision() {
-        guard let visionModel = try? VNCoreMLModel(for: imageClassifier().model)
-            else { fatalError("Can't load VisionML model") }
-        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
-        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
-        self.requests = [classificationRequest]
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.cameraLayer.frame = self.view.frame
     }
     
+    
+    //MARK: - CoreML Classifier and configurer
     func handleClassifications(request: VNRequest, error: Error?) {
         guard let observations = request.results
             else { print("no results: \(error!)"); return }
@@ -75,16 +84,63 @@ class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDeleg
     }
     
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        self.cameraLayer.frame = self.view.frame
+    func setupVision() {
+        if(downloaded) {
+            let fm = FileManager.default
+            let docsurl = try! fm.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let myurl = docsurl.appendingPathComponent("inceptionV3.mlmodel")
+            guard let compiledUrl = try? MLModel.compileModel(at: myurl) else {
+                fatalError("ruim")
+            }
+            guard let model = try? MLModel(contentsOf: compiledUrl) else {
+                fatalError("ruim")
+            }
+            
+            guard let visionModel = try? VNCoreMLModel(for: model)
+                else { fatalError("Can't load VisionML model") }
+            let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+            classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+            self.requests = [classificationRequest]
+            
+        } else {
+            guard let visionModel = try? VNCoreMLModel(for: imageClassifier().model)
+                else { fatalError("Can't load VisionML model") }
+            let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+            classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop
+            self.requests = [classificationRequest]
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - Core ML model Downloader
+    func downloadModel() {
+        
+        let urlString = "https://rapidwaveinc.com/drone/drone.mp4"
+        let destination = DownloadRequest.suggestedDownloadDestination(for: .documentDirectory)
+        
+        Alamofire.download(urlString, to: destination)
+            .downloadProgress { progress in
+                
+                let value = progress.fractionCompleted * 100
+                let rounded = round(value)
+                let final = rounded
+                self.uiDownloadLabel?.text = String(format: "%.1f",final) + "%"
+                
+                print("Download Progress: \(progress.fractionCompleted)")
+                self.uiSlider?.setValue(Float(progress.fractionCompleted), animated: true)
+            
+            }
+            .responseData { response in
+                if let data = response.result.value {
+                    print("file downloaded")
+                    self.downloaded = true
+                }
+        }
+
     }
     
+    //MARK: - Camera capture delegates
+
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
             return
@@ -104,10 +160,17 @@ class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDeleg
             print(error)
         }
     }
-
-
     
-
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+            else { return session }
+        session.addInput(input)
+        return session
+    }()
 
 }
 
